8c6d1c367c89566da8e60ec5a61cf3337661818c
vovabatsyk <vovabatsyk@gmail.com>
2023-02-22 18:24:10 +0200

    init

--- src/redux/types.js
+++
@@ -1,6 +0,0 @@
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
-export const ASYNC = 'ASYNC'
-export const CHANGE_THEME = 'CHANGE_THEME'
-export const DISABLE_BTN = 'DISABLE_BTN'
-export const ENABLE_BTN = 'ENABLE_BTN'
